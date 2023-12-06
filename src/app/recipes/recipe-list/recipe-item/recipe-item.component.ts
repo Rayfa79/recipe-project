134d3950820b941e3f0de4a8fb7cea86a6355ef8
Rayfa79 <96704831+Rayfa79@users.noreply.github.com>
2023-12-06 18:34:12 -0500

    used event and databinding to have a clicked item in the recipe list show as a recipe detail

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
+
 
 
 
@@ -10,4 +11,11 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent {
   @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
+
+
+  onClick() {
+    this.recipeSelected.emit()
+  }
+  
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  /*Pass the emitted event to the recipe-list selector nested in recipe component to let parent know what recipe was selected in order to then emit that event to the recipe-detail component selector **/
+  @Output() recipeListRecipeSelected =  new EventEmitter<Recipe>();
  recipes: Recipe[] = [
   new Recipe( 'Cajun Turkey Burger',  'Cajun cheeseburger with made with a special cajun seasoning topped with a a special spicy mayo with a toasted bun', 'https://divascancook.com/wp-content/uploads/2011/09/Still-1-728x430.jpeg'),
   new Recipe('Homemade Dinner Yeast Rolls', 'Pillowy-soft homeade yeast rolls that are so easy to make! Buttery with only a hing of sweetness makes these the perfect dinner rolls for any occasion',"https://divascancook.com/wp-content/uploads/2018/10/IMG_4575-728x546.jpg"),
   new Recipe('Spicy Crispy Fried Chicken','Crispy spicy fried chicken recipe thats full of cajun flavor and seasoned to perfection. A buttermilk brine ensures a juicy, tender piece of fried chicken everytime','https://divascancook.com/wp-content/uploads/2015/01/IMG_0231-750x469.jpg')
  ]
+
+ onRecipeSelected(recipe: Recipe) {
+  this.recipeListRecipeSelected.emit(recipe)
+ }
+
+ 
 }
